d290c1bea6cfd107a1745a790a94767850101e60
Joe Borg <joseph.borg@canonical.com>
2019-10-11 13:25:04 -0400

    Send pause image path to containerd charm (#1)
    * Send pause image path to containerd charm
    
    * tweak to use manage_flags and be explicit about individual config getters/setters

--- provides.py
+++ provides.py
@@ -1,28 +1,55 @@
 from charms.reactive import (
     Endpoint,
-    set_flag,
-    clear_flag
-)
-
-from charms.reactive import (
-    when,
-    when_not
+    toggle_flag
 )
 
 
 class ContainerRuntimeProvides(Endpoint):
-    @when('endpoint.{endpoint_name}.joined')
-    def joined(self):
-        set_flag(self.expand_name('endpoint.{endpoint_name}.available'))
+    def manage_flags(self):
+        toggle_flag(self.expand_name('endpoint.{endpoint_name}.available'),
+                    self.is_joined)
 
-    @when_not('endpoint.{endpoint_name}.joined')
-    def broken(self):
-        clear_flag(self.expand_name('endpoint.{endpoint_name}.available'))
-
-    def get_config(self):
+    def _get_config(self, key):
         """
-        Get the configuration published.
+        Get the published configuration for a given key.
 
-        :return: Dictionary configuration
+        :param key: String dict key
+        :return: String value for given key
         """
-        return self.all_joined_units.received
+        return self.all_joined_units.received.get(key)
+
+    def get_nvidia_enabled(self):
+        """
+        Get the published nvidia config.
+
+        :return: String
+        """
+        return self._get_config(key='nvidia_enabled')
+
+    def get_runtime(self):
+        """
+        Get the published runtime config.
+
+        :return: String
+        """
+        return self._get_config(key='runtime')
+
+    def get_socket(self):
+        """
+        Get the published socket config.
+
+        :return: String
+        """
+        return self._get_config(key='socket')
+
+    def set_config(self, sandbox_image=None):
+        """
+        Set the configuration to be published.
+
+        :param sandbox_image: String to optionally override the sandbox image
+        :return: None
+        """
+        for relation in self.relations:
+            relation.to_publish.update({
+                'sandbox_image': sandbox_image
+            })
--- requires.py
+++ requires.py
@@ -1,23 +1,48 @@
 from charms.reactive import (
     Endpoint,
-    set_flag,
-    clear_flag
-)
-
-from charms.reactive import (
-    when,
-    when_not
+    clear_flag,
+    data_changed,
+    is_data_changed,
+    toggle_flag
 )
 
 
 class ContainerRuntimeRequires(Endpoint):
-    @when('endpoint.{endpoint_name}.changed')
-    def changed(self):
-        set_flag(self.expand_name('endpoint.{endpoint_name}.available'))
+    def manage_flags(self):
+        toggle_flag(self.expand_name('endpoint.{endpoint_name}.available'),
+                    self.is_joined)
+        toggle_flag(self.expand_name('endpoint.{endpoint_name}.reconfigure'),
+                    self.is_joined and self._config_changed())
 
-    @when_not('endpoint.{endpoint_name}.joined')
-    def broken(self):
-        clear_flag(self.expand_name('endpoint.{endpoint_name}.available'))
+    def _config_changed(self):
+        """
+        Determine if our received data has changed.
+
+        :return: Boolean
+        """
+        # NB: this call should match whatever we're tracking in handle_remote_config
+        return is_data_changed('containerd.remote_config',
+                               [self.get_sandbox_image()])
+
+    def handle_remote_config(self):
+        """
+        Keep track of received data so we can know if it changes.
+
+        :return: None
+        """
+        clear_flag(self.expand_name('endpoint.{endpoint_name}.reconfigure'))
+        # Presently, we only care about one piece of remote config. Expand
+        # the list as needed.
+        data_changed('containerd.remote_config',
+                     [self.get_sandbox_image()])
+
+    def get_sandbox_image(self):
+        """
+        Get the sandbox image URI if a remote has published one.
+
+        :return: String: remotely configured sandbox image
+        """
+        return self.all_joined_units.received.get('sandbox_image')
 
     def set_config(self, socket, runtime, nvidia_enabled):
         """
